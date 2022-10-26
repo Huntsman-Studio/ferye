ebefd65fdc82bc8b742c6e38710c4686b95bf4cd
Kristel Lamaj <kri.lamaj@gmail.com>
2022-10-26 13:17:45 +0300

    extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "darkForest": "#002828",
+        "clover": "#004F4F",
+        "pine": "#007377",
+        "jungle": "#009B97",
+        "seafom": "#00C1C1",
+        "pistachio": "#B8E1DC",
+        "ice": "#E0F6F3",
+        "amarant": "#EF3F4C",
+        "salmon": "#FFCCCC",
+        "grey": {
+          300: '#282828',
+          200: '#6D6D6D',
+          100: '#8F8F8F'
+        },
+        "goldGrey": "#B1B1B1",
+        "Grey": {
+          100: '#D3D3D3',
+          200: '#E8E8E8',
+          300: '#F5F5F5'
+        },
+        "coconut": '#FAFAFA'
+      }
+    },
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('flowbite/plugin')
+  ],
 }
