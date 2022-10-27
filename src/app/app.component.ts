81ba1fd3acb72d5d9a1c0297e139e5c34688e600
klamaj <kri.lamaj@gmail.com>
2022-10-27 16:39:05 +0300

    Favicons for light and dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFaviconService } from 'angular-favicon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ferye';
+
+  constructor(private ngxFavicon: AngularFaviconService) {}
+
+  ngOnInit() {
+    this.ngxFavicon.setFavicon("assets/images/icons/ferye_favicon_dark.svg", "assets/images/icons/ferye_favicon_light.svg");
+  }
 }
