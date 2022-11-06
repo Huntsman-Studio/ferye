77db0825c6782cc2cb6f010419b5c28501f00b20
Kristel Lamaj <kri.lamaj@gmail.com>
2022-11-06 21:28:29 +0200

    icons mofule

---
+++ src/app/shared/icons/ferye-logo-dark/ferye-logo-dark.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ferye-logo-dark',
+  templateUrl: './ferye-logo-dark.component.html',
+  styleUrls: ['./ferye-logo-dark.component.scss']
+})
+export class FeryeLogoDarkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/icons/icons.module.ts
+++ src/app/shared/icons/icons.module.ts
@@ -2,20 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DangerComponent } from './danger/danger.component';
 import { CheckComponent } from './check/check.component';
+import { CartComponent } from './cart/cart.component';
+import { FavouritesIconComponent } from './favourites-icon/favourites-icon.component';
+import { MenuIconComponent } from './menu-icon/menu-icon.component';
+import { SearchIconComponent } from './search-icon/search-icon.component';
+import { FeryeLogoDarkComponent } from './ferye-logo-dark/ferye-logo-dark.component';
 
 
 
 @NgModule({
   declarations: [
     DangerComponent,
-    CheckComponent
+    CheckComponent,
+    CartComponent,
+    FavouritesIconComponent,
+    MenuIconComponent,
+    SearchIconComponent,
+    FeryeLogoDarkComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     DangerComponent,
-    CheckComponent
+    CheckComponent,
+    CartComponent,
+    FavouritesIconComponent,
+    MenuIconComponent,
+    SearchIconComponent,
+    FeryeLogoDarkComponent
   ]
 })
 export class IconsModule { }
