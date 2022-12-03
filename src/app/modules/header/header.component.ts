5ab1c0acf19234abb71ef26547935478821c391f
Kristel Lamaj <kri.lamaj@gmail.com>
2022-12-03 16:45:38 +0200

    header-bg

--- src/app/modules/header/header.component.ts
+++ src/app/modules/header/header.component.ts
@@ -9,6 +9,8 @@ import { GlobalDataService } from 'src/app/core/services/global-data.service';
 })
 export class HeaderComponent implements OnInit {
 
+  public element = document.getElementById('');
+
   constructor(public router: Router, private globalDataService: GlobalDataService) { }
 
   ngOnInit(): void {
@@ -17,5 +19,18 @@ export class HeaderComponent implements OnInit {
   // showMenu
   showMenu() {
     this.globalDataService.showHideMenu(true);
+  }
+
+  // set bg Color 
+  setElementClass(): string {
+    if (this.router.url.includes('login')) {
+      return 'FAFAFA';
+    }
+    else if (this.router.url.includes('contact')) {
+      return '#E0EBF6';
+    }
+    else {
+      return '#E0F6F3';
+    }
   }
 }
